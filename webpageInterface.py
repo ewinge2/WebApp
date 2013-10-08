00b1fe728fa9d30ecfd2a659acfc3f5d085e3f11
Jialun <gzmicrosnail@gmail.com>
2013-10-07 23:41:55 -0500

    Update webpageInterface.py

--- webpageInterface.py
+++ webpageInterface.py
@@ -6,23 +6,27 @@ class webpageInterface:
 	def __init__(self):
 		self.initialContent = open('CarlStats.html').read()
 		self.major = ''
-		self.year = ''
+		self.startYear = ''
 		self.debug = ''
 
 	def getInput(self):
 		form = cgi.FieldStorage()
+		if 'startYear' in form:
+			self.startYear = form['startYear'].value
+			self.debug = self.startYear
 
 	def generate(self):
 		self.getInput()
 		print "Content-type: text/html\r\r\n\n"  
-# 		(self.major, self.year)
-		output = self.initialContent % (self.major, self.year, self.debug)
+		output = self.initialContent % (self.major, self.startYear, self.debug)
 		print output 
 
 if __name__ == "__main__":	
 	site = webpageInterface()
 	site.generate()
 
+# 	site.getInput()
+# 	site.generate()
 	
 
 

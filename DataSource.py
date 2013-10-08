4b6ce1c86f744f47511f12bb5f1489f287633f09
AGrosse <paigefox1901@gmail.com>
2013-10-07 22:24:52 -0500

    Update DataSource.py
    added method to get number of graduates of a certain major over a range of years, rather than just one year at a time or all years.
    also method to do this for total number of degrees awarded.

--- DataSource.py
+++ DataSource.py
@@ -3,33 +3,47 @@ class DataSource:
         pass
     
     def get_all_graduates_from_year(self, year, gender = 'all'):
-        """Returns a dictionary of the number of graduates with each 
-           major in a given year. Each key is a major, with value equal 
-           to the number of graduates with that major and with the 
-           specified gender.
+        """Returns a dictionary with keys equal to each major in the dataset
+           and values equal to the number of graduates with specified gender
+           and with majors that are the corresponding key.
            
            year is an integer. Throws an exception if the specified year 
            isn't a year in our dataset.
+           
            gender is a string. Throws an exception if specified gender 
            isn't 'male', 'female', or 'all'. If gender is all, graduates of 
            any gender are considered."""
         return {}
         
     def get_graduates_from_all_years(self, major = 'all', gender = 'all'):
-        """Returns a dictionary of the total number of graduates with a 
-           given major in all years in the dataset. Each key 
-           is a year, with value equal to the number of graduates with 
-           the specified major in that year. If no major was specified, the 
-           dictionary contains values equal to the total number of graduates 
-           in a year.
+        """Returns a dictionary with keys equal to each year in the dataset
+           and values equal to the number of graduates with 
+           the specified major and specified gender in a year.
            
            major is a string. Throws an exception if the specified major isn't 
            a major in our dataset. If major is 'all', graduates of any major 
            are considered.
+           
            gender is a string. Throws an exception if the specified gender
            isn't 'male', 'female', or 'all'. If gender is all, graduates of 
            any gender are considered."""
         return {}
+        
+    def get_graduates_in_year_range(self, year_start, year_end, major = 'all', gender = 'all'):
+        """Returns a dictionary of the total number of graduates with a given
+           major and given gender in all years in the range specified by the user.
+           
+           year_start and year_end are integers. year_start and year_end specify
+           the range of years for which the dictionary includes information on graduates.
+           Throws an exception if year_start or year_end are not years in our dataset.
+           
+           major is a string. Throws an exception if the specified major isn't 
+           a major in our dataset. If major is 'all', graduates of any major 
+           are considered.
+           
+           gender is a string. Throws an exception if the specified gender
+           isn't 'male', 'female', or 'all'. If gender is all, graduates of 
+           any gender are considered."""
         
     def get_graduates_from_year(self, year, major = 'all', gender = 'all'):
         """Returns the number of graduates in a given year with a given major
@@ -37,38 +51,47 @@ class DataSource:
            
            year is an integer. Throws an exception if the specified year 
            isn't a year in our dataset.
+           
            major is a string. Throws an exception if the specified major isn't 
            a major in our dataset. If major is 'all', graduates of any major 
            are considered.
+           
            gender is a string. Throws an exception if the specified gender
            isn't 'male', 'female', or 'all'. If gender is all, graduates of 
            any gender are considered."""
         return 0
     
     def get_number_of_degrees_from_year(self, year):
-        """Returns the total number of degrees awarded in any major. Note
-           that this is different from the total number of graduates:
+        """Returns the total number of degrees awarded in any major in a given 
+           year. Note that this is different from the total number of graduates:
            some graduates may be awarded degrees in mutliple majors.
            
            year is an integer. Throws an exception if the specified year 
            isn't a year in our dataset."""
         return 0
-        
-        
-    def get_number_of_degrees_from_year_span(self, startYear, endYear, step = 1):
-        '''
-        @return :return a list whose keys are years (int) and whose values are numbers of degrees
-        '''
-        
-        return []
     
     def get_number_of_degrees_from_all_years(self):
-        """Returns a list of the total number of degrees awarded in any
-           major for all years in the dataset. Note that this is 
-           different from the total number of graduates: some graduates 
-           may be awarded degrees in mutliple majors."""
-        return []
+        """Returns a dictionary of the total number of degrees awarded in any
+           major for all years in the dataset. Keys are years and values are
+           the corresponding number of degrees awarded. Note that the total
+           number of degrees awarded is different from the total number of 
+           graduates because some graduates may be awarded degrees in mutliple 
+           majors."""
+        return {}
+    
+    def get_number_of_degrees_in_year_range(self, year_start, year_end):
+        """Returns a dictionary of the total number of degrees awarded in any
+           major for all years in the user-specified range. Keys are years and 
+           values are the corresponding number of degrees awarded. Note that the
+           total number of degrees awarded is different from the total number of 
+           graduates because some graduates may be awarded degrees in mutliple 
+           majors.
            
+           year_start and year_end are integers. year_start and year_end 
+           specify the range of years for which the dictionary contains information
+           on degrees. Throws an exception if year_start or year_end are not 
+           years in our dataset."""
+    
     def get_majors(self):
         """Returns a list of all majors in the dataset."""
         return []
